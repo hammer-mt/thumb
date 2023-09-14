df04ca57c17b977325fde48a527f2304159936f7
jamesaphoenix <jamesaphoenix@googlemail.com>
2023-09-14 16:24:39 +0100

    Adding doc strings.

--- src/thumb/utils.py
+++ src/thumb/utils.py
@@ -1,7 +1,16 @@
 import hashlib
 
-def hash_id(string):
-    # hash to get a unique id that will be the same if passed the same string
+
+def hash_id(string) -> str:
+    """
+    Hashes a string using the MD5 algorithm and returns the first 8 characters of the resulting hash.
+
+    Args:
+        string (str): The string to be hashed.
+
+    Returns:
+        str: The first 8 characters of the MD5 hash of the input string.
+    """
     hash = hashlib.md5(string.encode()).hexdigest()
     hash_id = hash[:8]
     return hash_id
